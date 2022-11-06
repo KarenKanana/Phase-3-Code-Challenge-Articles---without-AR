cc98cb3a6cfe1bb42e4d3bbc0d24798d679093b6
Karen Kanana <kananakaren21@gmail.com>
2022-11-06 18:49:20 +0300

    Complete code challenge

--- tools/console.rb
+++ tools/console.rb
@@ -7,10 +7,20 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+### Author class initialization test
+nana = Author.new("Nana")
+karen = Author.new("Karen")
+kanana = Author.new("Kanana")
 
+### Magazine class initialization test
+new_york_times = Magazine.new("New York Times", "News")
+forbes = Magazine.new("Forbes", "Entertainment")
+top_forty_under_forty = Magazine.new("Top 40 Under 40", "Inspirational")
 
-
-
+### Article class initialization test
+nana.add_article(entertainment, "5 Facts About Me You Didn't Know")
+karen.add_article(education, "The Ultimate Developers Guide")
+kanana.add_article(health, "Why you need to start working out")
 
 
 
